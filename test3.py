606c676026511c3c3eca7d877cd8332cbff21578
Kyungsik Kim <kyungsik@rnd.re.kr>
2024-01-25 11:43:00 +0900

    Revert "Add 3"
    This reverts commit 687374a49c5e3b9a1fe9114df0703a9643992100.

--- test3.py
+++ test3.py
@@ -1,12 +1,5 @@
 # what is this1
 # what is this2
 # add text
-<<<<<<< HEAD
-<<<<<<< HEAD
 # 1
-# 2
-# 3
-=======
->>>>>>> parent of 9f39f28 (Add 1)
-=======
->>>>>>> parent of 9f39f28 (Add 1)
+# 10
